da0ddde547f61812e863204d5a853516f2aabb4a
Ash <ash315ia@gmail.com>
2021-04-05 21:12:40 +0400

    Repasted web console of game

---
+++ runcode.js
@@ -0,0 +1,56 @@
+playerScore = 0; // variable to hold the player's score
+computerScore = 0; // variable to hold the computer's score
+const value = ["Rock", "Scissors", "Paper"]; // an array of answers to choose from, for the computer
+
+// this function is used to get a random answer from the "value[]" array
+function computerPlay() {
+    let randomvalue = value[Math.floor(Math.random() * value.length)]; 
+    return randomvalue;
+}
+
+// rounds function decides whether player or computer wins a single round (depending on answers provided) and returns the result of the same
+function rounds(playerSelection) {
+    let computerSelection = computerPlay(); // function called to get random answer from "value[]" array
+    let result = ""; // result which is returned and will contain the outcome of the round
+    // this "if" statement says the player won 
+    if( (playerSelection == "Rock" && computerSelection == "Scissors") || (playerSelection == "Scissors" && computerSelection == "Paper") || (playerSelection == "Paper" && computerSelection == "Rock")) {
+        playerScore++;
+        result = "Congratulations! You won this round as " + playerSelection + " beats " + computerSelection + ". Your score is " + playerScore + " and computer score is " + computerScore + ".";
+    }
+    // this statement says the player lost
+    else if( (playerSelection == "Scissors" && computerSelection == "Rock") || (playerSelection == "Paper" && computerSelection == "Scissors") || (playerSelection == "Rock" && computerSelection == "Paper") ){
+        computerScore++;
+        result = "You lose! Because " + computerSelection + " beats " + playerSelection + ". Computer score is " + computerScore + " and your score is " + playerScore;
+    }
+    // the round ended in a tie
+    else {
+        result = "This round is a tie because " + playerSelection + " doesn't beat nor lose to itself. Your score is " + playerScore + " and computer score is " + computerScore;
+    }
+    return result;
+}
+
+// "game()" is where the user inputs their answer and the overall outcome of the game is decided
+function game() {
+    for(var i = 0; i < 5; i++) {
+        let userValue = prompt("Please enter your choice");
+        if ( (userValue == "Rock") || (userValue == "Paper") || (userValue == "Scissors") ) { // ensures that answers provided aren't "false"
+            console.log(rounds(userValue)) // returns the outcome of each round;
+        }
+        else {
+            console.log("This answer is invalid. Please reload the game and input valid choices.")
+            return;
+        }
+    }
+    // following statements print the end outcome of the overall game
+    if(playerScore > computerScore) {
+        console.log("Congratulations, you won the game!")
+    }
+    else if(playerScore < computerScore) {
+        console.log("Aww, you lost :( Better luck next time.")
+    }
+    else {
+        console.log("This game has ended in a draw. Maybe next time...");
+    }
+}
+
+game(); // "game()" function is called and is where everything starts
